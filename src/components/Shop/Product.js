14fc1a17b7a19790e992f8a429ecb0177a2b876f
unknown <stvhrsdev>
2025-09-16 02:14:14 +0700

    related

--- src/components/Shop/Product.js
+++ src/components/Shop/Product.js
@@ -7,7 +7,7 @@ import ProductImageDescription from "./ProductImageDescription";
 // 1. Impor yang sudah diperbarui: Hanya dari satu file SEO terpusat.
 // Pastikan path-nya sudah sesuai dengan struktur folder Anda.
 import SEO, { ldProduct, ldBreadcrumb } from "../seo";
-import RelatedProductSlider from "./RelatedProductSlider";
+import SameSubjectProductSlider from "./RelatedProductSlider";
 
 const Breadcrumb = React.lazy(() => import("../Breadcrumb"));
 const ORIGIN = "https://www.fajargrafika.com";
@@ -47,6 +47,7 @@ const Product = () => {
   const images = (product.image || []).map(abs);
   const bookWeight = parseWeight(product.specifications.weight);
   const bookFormat = getBookFormat(product.specifications.binding);
+  const productMapel = product.tag && product.tag.length > 0 ? product.tag[0] : null;
 
   return (
     <Fragment>
@@ -54,7 +55,7 @@ const Product = () => {
       <SEO
         // Meta Tags Dasar & Open Graph
         title={product.name}
-     
+
         description={product.shortDescription}
         image={images[0]}
         canonical={canonicalUrl}
@@ -79,7 +80,7 @@ const Product = () => {
 
             // Properti Harga
             price: product.price,
-          
+
 
             // Properti Rating
             ratingValue: product.rating,
@@ -98,15 +99,19 @@ const Product = () => {
           }),
         ]}
       />
-{/* <RelatedProductSlider  /> */}
-      {/* Sisa konten halaman Anda */}
+
       <Suspense fallback={<Preloader />}>
         <Breadcrumb title={product.name} useKatalog={true} />
         <ProductImageDescription
           spaceTopClass="pt-100"
           spaceBottomClass="pb-100"
           product={product}
+        />   <SameSubjectProductSlider
+          spaceBottomClass="pb-95"
+          currentProductMapel={productMapel}
+          currentProductId={product.id}
         />
+      {/* Sisa konten halaman Anda */}
       </Suspense>
     </Fragment>
   );
